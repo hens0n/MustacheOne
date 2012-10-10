d4efc14227f746396e5aef59a0bfc2517153d4de
Jacob Henson <cyberphilia@gmail.com>
2012-10-10 13:17:59 -0500

    updage GetDomainInfo in networksolutions

--- MustacheOne.py
+++ MustacheOne.py
@@ -10,11 +10,24 @@ from mustacheone import projecthoneypot
 from mustacheone import robtex
 
 def ReportHeader(query):
+	header ="""
+___  ___          _             _           _____            
+|  \\/  |         | |           | |         |  _  |           
+| .  . |_   _ ___| |_ __ _  ___| |__   ___ | | | |_ __   ___ 
+| |\\/| | | | / __| __/ _` |/ __| '_ \\ / _ \\| | | | '_ \\ / _ \\
+| |  | | |_| \\__ \\ || (_| | (__| | | |  __/\\ \\_/ / | | |  __/
+\\_|  |_/\\__,_|___/\\__\\__,_|\\___|_| |_|\\___| \\___/|_| |_|\\___|                                                            
+                                                             
+	https://github.com/cyberphilia/MustacheOne
+"""
 	now = datetime.datetime.now()
+	print(header)
 	print("==========================================================")
 	print("Report for " + query)
 	print(now)
 	print("==========================================================")
+                                                           
+                                                             
 
 def IsIp(query):
 	try:
--- mustacheone/networksolutions.py
+++ mustacheone/networksolutions.py
@@ -32,3 +32,26 @@ def GetDomainInfo(Domain):
 		cols = row.findAll('td')
 		if type(cols[1].string).__name__ !=  'NoneType':
 			print(cols[0].string + ' ' +cols[1].string.strip())
+			
+	theContent = soup.find("div", {"class":"content"})
+	thetable = theContent.find("table")
+	theSubTable = thetable.find("table")
+	cols = theSubTable.findAll('td')
+	for col in cols:
+		c = col.renderContents().strip()
+		if len(c) > 4	:		
+			if col.strong != None:
+				
+				c = c.replace('\n','')
+				c = c.replace('\t','')
+				c = c.replace('<strong>','')
+				c = c.replace('</strong>','')
+				print("")
+				print("*"+c)
+			elif  col.a != None:
+				if 'target' in col.a.attrs and not "underlying" in col.a.renderContents().strip():
+					print(col.a.renderContents().strip())
+			elif  col.pre != None:
+				print(col.pre.renderContents().strip())		
+			else:
+				print(c.strip())
